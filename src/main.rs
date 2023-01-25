f8a47b28c656069e7f882649bef4aa486400c1fc
zbrachinara <zbrachinara@gmail.com>
2023-01-24 23:08:43 -0500

    basic representation of board

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use nannou::prelude::*;
 
-struct Model {}
+enum Player {
+    None,
+    Player1,
+    Player2,
+    Player3,
+}
+
+/// Board is a 2d representation of the hexagonal grid, where the horizontal component remains as is
+/// and the "vertical" component is the rightward shearing line. The leftward shearing line can be
+/// represented using left diagonal lines drawn through the "gridlines". Right diagnonal lines
+/// should *not* be used for any purpose regarding "local" (regarding neighboring cells) behavior.
+/// 
+/// The center of the coordinate system refers to the cell in the direct center of the board. From
+/// there, the point (0, 4) is in the top right corner within the central hexagon, while (0, -4) is
+/// in the bottom left corner of the hexagon. Similarly, the point at (4, 0) is at the right, while 
+/// (-4, 0) is in the left. By this, the top left is at (-4, 4) and the bottom right is at (4, -4)
+struct Board {
+    backing: HashMap<IVec2, Player>,
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        Self { backing: Default::default() }
+    }
+}
+
+impl Board {
+    pub fn draw(&self, draw: &Draw) {
+
+    }
+}
+
+#[derive(Default)]
+struct Model {
+    board: Board,
+}
 
 fn main() {
     nannou::app(model).simple_window(window_handler).run()
-    // nannou::sketch(window_handler).run();
 }
 
 fn model(_: &App) -> Model {
-    Model {}
+    Model::default()
 }
 
 fn window_handler(app: &App, _: &Model, f: Frame) {
