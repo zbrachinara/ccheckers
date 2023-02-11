778ae7b07f5aa6952828e08e447257d69eaf0fea
zbrachinara <zbrachinara@gmail.com>
2023-02-11 12:34:41 -0500

    minimal ability to run in web browser

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
 #![allow(unused)]
 
 use async_std::task::block_on;
+use nannou::{prelude::*, wgpu::Backends};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 mod common;
 
-// web app entry_point
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 pub async fn main_web() {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
     block_on(async {
+        app::Builder::new_async(|app| Box::new(common::model(app)))
+            .backends(Backends::PRIMARY | Backends::GL)
+            .event(common::events)
+            .run_async()
+            .await
     });
 }
