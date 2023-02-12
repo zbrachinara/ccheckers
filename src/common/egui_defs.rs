828890c10f579d60fb91dcd85d62a346c1828ba9
zbrachinara <zbrachinara@gmail.com>
2023-02-12 09:30:13 -0500

    implement saving/loading games on desktop

--- src/common/egui_defs.rs
+++ src/common/egui_defs.rs
@@ -1,9 +1,16 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+};
+
 use super::{
+    board::Board,
     player::{Mode, Turn},
     Model,
 };
 use nannou::prelude::*;
 use nannou_egui::egui;
+use rfd::FileDialog;
 use strum::IntoEnumIterator;
 
 #[derive(Default)]
@@ -11,6 +18,23 @@ pub struct EguiData {
     mode: Mode,
 }
 
+fn load_game() -> Option<Board> {
+    FileDialog::new()
+        .pick_file()
+        .and_then(|path| fs::read_to_string(path).ok())
+        .and_then(|str| ron::from_str(&str).ok())
+}
+
+fn save_game(board: &Board) {
+    if let Some(file_handle) = FileDialog::new()
+        .set_file_name("unnamed_ccheckers_game.ron")
+        .save_file()
+    {
+        if let Ok(mut f) = File::create(file_handle) {
+            f.write_all(ron::to_string(board).unwrap().as_bytes());
+        }
+    }
+}
 pub fn define_ui(model: &mut Model, update: &Update) {
     let Model { ref mut egui, .. } = *model;
     egui.set_elapsed_time(update.since_start);
@@ -28,9 +52,21 @@ pub fn define_ui(model: &mut Model, update: &Update) {
                     ui.selectable_value(&mut model.egui_data.mode, mode, format!("{mode}"));
                 }
             });
-        if ui.button("Reset field").clicked() {
-            model.board.reset(model.egui_data.mode);
-        }
+        ui.horizontal(|ui| {
+            if ui.button("Reset field").clicked() {
+                model.board.reset(model.egui_data.mode);
+            }
+
+            if ui.button("Save game").clicked() {
+                save_game(&model.board);
+            }
+
+            if ui.button("Load game").clicked() {
+                if let Some(board) = load_game() {
+                    model.board = board;
+                }
+            }
+        });
 
         if model.board.turn != Turn::None {
             ui.label(format!("Currently {}'s turn", model.board.turn));
